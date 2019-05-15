cd4a742452e3f52932acb0f1cdab429007021158
sappy <rtw@stronzi.org>
2019-05-15 15:34:40 -0400

    first
    newer info

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteTitle: 'Sean Business', // Site title.
+  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'rtw1.netlify.com', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: 'Family Mediation and Counseling', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: 'rtw1', // Disqus shortname.
+  userName: 'Sean Testing',
+  userTwitter: 'None',
+  userLocation: 'Ottawa, ON, Canada',
+  userDescription: '',
+  copyright: 'Copyright © rtw1.netflify.com 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b3', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+}
